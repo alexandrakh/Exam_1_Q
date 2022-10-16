9f650b81fd5a575bca5426f7279bb5c0803da360
Aleksandra <kharchenko.aleksandra@yandex.ru>
2022-10-16 23:31:18 +0300

    Add Task

---
+++ Task/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите массив строк через запятую");
+string str = Console.ReadLine();
+int count = 0;
+int sizeStr = 3;
+
+string[] array;
+array = str.Split(',');
+
+Console.WriteLine(string.Join(',', GetArrayWithSizeStr(array), 0, count));
+
+string[] GetArrayWithSizeStr(string[] array)
+{
+    string[] result = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= sizeStr)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    return result;
+}
